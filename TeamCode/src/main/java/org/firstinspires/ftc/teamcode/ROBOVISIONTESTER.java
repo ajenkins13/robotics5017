ba495bd05b932536c71d9fb2f6fd8c6447510118
tolympia <tozhu3@gmail.com>
2020-12-14 16:04:07 -0500

    Tester class for robot vision (fixed).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOVISIONTESTER.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROBOVISIONTESTER.java
@@ -26,8 +26,6 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.firstinspires.ftc.robotcontroller.external.samples;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import java.util.List;
